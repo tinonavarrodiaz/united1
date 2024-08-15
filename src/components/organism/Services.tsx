bf804a8ef4bab7aa2a877e90a2affa2201114adc
Florentino Navarro <tino.navarro@hotmail.com>
2024-08-15 10:36:38 -0600

    first commit

--- src/components/organism/Services.tsx
+++ src/components/organism/Services.tsx
@@ -5,6 +5,10 @@ export default function Services() {
   return (
     <section className=''>
       <Img src="land" ext="jpg" alt="Services Image" ></Img>
+      <h2>Services</h2>
+      <p>United Beverages Warehouse is the <br/>
+home for all the one of a kind items <br/>
+that make up our portfolio.</p>
     </section>
   )
 }
